4ccd5a837d6ccb749120fc882c9cd9d734dfc488
Nader Sayed <nader19113118@gmail.com>
2025-03-14 05:21:24 +0200

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:image_compression/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    print(e.toString());
+  }
+
   runApp(const MyApp());
 }
 
