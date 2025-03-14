47f476162c06f4904dc337ec52dce8362dc2a25d
Nader Sayed <nader19113118@gmail.com>
2025-03-14 04:24:03 +0200

    Updates

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Home Screen",
+        ),
+      ),
+    );
+  }
+}
